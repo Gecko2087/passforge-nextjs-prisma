d38a4e6d10cd848a4a987566065fe0c560a70b4d
Lucas Nieto <lucasnieto22@gmail.com>
2025-12-10 21:55:28 -0300

    Actualizar página principal del dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
 import FormCreatePassword from "./_components/form-create-password";
+import PasswordLists from "./_components/password-list";
 
 const DashboardPage = () => {
     return (
     <div className="container mx-auto px-4">
         <FormCreatePassword />
+        <PasswordLists />
     </div>)
 }
 
